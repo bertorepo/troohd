41da5a9650f0803dbdec702968ba6331c60aac64
Hubert Lemuel <bert.stark3231@gmail.com>
2021-09-24 13:50:07 -0700

    committed

---
+++ troohd-app/src/main/java/com/troohd/app/TroohdAppApplication.java
@@ -0,0 +1,13 @@
+package com.troohd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroohdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroohdAppApplication.class, args);
+	}
+
+}
---
+++ troohd-app/src/test/java/com/troohd/app/TroohdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troohd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroohdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
